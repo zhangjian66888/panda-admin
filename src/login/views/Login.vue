75316216cda36767d5047ef8d3f8e70c0727d52a
zhanglijian <zhanglijian@vipkid.com.cn>
2019-07-05 14:34:00 +0800

    init

--- src/login/views/Login.vue
+++ src/login/views/Login.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>
+  <div class="pd-login">
     this is new login page
   </div>
 </template>
+<style scoped>
+
+  .pd-login {
+    background: url("../../assets/images/login-bg.jpeg");
+  }
+
+</style>
